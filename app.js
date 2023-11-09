bebdafc5df7819a42fa30b7e100c8846e9681828
ramiro125 <ramiro12526@gmail.com>
2023-11-09 00:10:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const port = process.env.PORT || 4000
+
+app.listen(port, ()=>{
+    console.log(
+    `Servidor funcionando en el puerto ${port}`);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
